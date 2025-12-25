1309ad10b4c2119913cfb99623cf6703579e5917
Maciej Gdula <maciekzgk@gmail.com>
2025-12-25 18:35:44 +0100

    Added current_value of shares, added shares summary

--- src/subpages/wallet/Stock.jsx
+++ src/subpages/wallet/Stock.jsx
@@ -10,14 +10,19 @@ class AssetData {
     constructor(quantity, price) {
         this.quantity = quantity;
         this.price = price;
+        this.cost = this.price * this.quantity;
     }
     // get profit in percentages, relative to current price
-    getPercentageProfit(current_price) {
-        return (((current_price) - this.price) / this.price) * 100;
+    getProfitPercentage(current_price) {
+        return (((current_price * this.quantity) - this.cost) / this.cost) * 100;
     }
     // get profit relative to current price
     getProfit(current_price) {
         return (current_price - this.price) * this.quantity;
+    }
+    // count current value of all shares relative to its current price
+    getCurrentValue(current_price) {
+        return (current_price * this.quantity);
     }
 };
 
@@ -55,8 +60,8 @@ class AssetsEntry {
         }
 
         // update averages
-        var amount = 0.0; // summed amount of owned resource
-        var prices_cummulated = 0.0;
+        let amount = 0.0; // summed amount of owned resource
+        let prices_cummulated = 0.0;
 
         this._data.forEach((e) => {
             amount += e.quantity;
@@ -66,17 +71,25 @@ class AssetsEntry {
         this.folded_data[0] = new AssetData(amount, prices_cummulated / amount);
     }
 
+    get average_data() {
+        return this.folded_data[0];
+    }
+
+    get accumulated_cost() {
+        return this.average_data.cost;
+    }
+
+    get current_value() {
+        return this.average_data.getCurrentValue(this.current_price);
+    }
+
     // get profit from all transactions of shares
     get profit() {
         return this.folded_data[0].getProfit(this.current_price);
     }
     // get percentage profit from all transactions of shares
     get profit_percentage() {
-        return this.folded_data[0].getPercentageProfit(this.current_price);
-    }
-
-    get average_data() {
-        return this.folded_data[0];
+        return this.folded_data[0].getProfitPercentage(this.current_price);
     }
 
     get data() {
@@ -89,11 +102,150 @@ class AssetsEntry {
     }
 };
 
+// data prepared for table to be rendered
+class AssetsTableData {
+    /// folded (bool), ticker(string), idx (int, idx of data), data(AssetData)
+    constructor(ticker, isFolded, isFirst, quantity, current_price, current_value, profit, profit_percentage, price, cost) {
+        this.ticker = ticker;
+        this.isFolded = isFolded;
+        this.isFirst = isFirst;
+        this.quantity = quantity; 
+        this.current_price = current_price;
+        this.current_value = current_value;
+        this.profit = profit;
+        this.profit_percentage = profit_percentage;
+        this.cost = cost,
+        this.price = price;
+    }
+};
+
+/// Map consisting assets
+class AssetsMap {
+    constructor(other) {
+        if (other instanceof AssetsMap) {
+            this.asset_map = new Map(other.asset_map);
+            this.summary = other.summary;
+        }
+        else {
+            this.asset_map = new Map();
+        }
+    }
+    // return array of AssetsTableData produced based on personal assets
+    produceTableData() {
+        let output_array = []
+        this.asset_map.forEach((asset) => {
+            asset.data.forEach((data, idx) => {
+                output_array.push(
+                    new AssetsTableData(
+                        asset.ticker, 
+                        asset.folded, 
+                        idx === 0,
+                        data.quantity,
+                        asset.current_price,
+                        asset.current_value,
+                        data.getProfit(asset.current_price).toFixed(2),
+                        data.getProfitPercentage(asset.current_price).toFixed(2),
+                        data.price.toFixed(2),
+                        data.cost,
+                    )
+                );
+            })
+        });
+        return output_array;
+    }
+    // set resource for given ticker
+    set(ticker, map_entry) {
+        this.asset_map.set(ticker, map_entry);
+        this.countAssetsSummary(); // we should re-count asset's summary after updating the map
+    }
+    // get resource associated to ticker from map
+    get(ticker) {
+        return this.asset_map.get(ticker);
+    }
+    // check if ticker exists in map
+    has(ticker) {
+        return this.asset_map.has(ticker);
+    }
+    // prepare summary of all assets in map
+    countAssetsSummary() {
+        let summary = new AssetsTableData();
+        let isFirst = false;
+        let isFolded = false;
+        let ticker = "Summary";
+        let quantity = 0;
+        let cost = 0;
+        let profit = 0.0;
+        let current_value = 0.0;
+        this.asset_map.forEach((entry) => {            
+            const e = entry.average_data;
+            // price += e.price;
+            quantity += e.quantity;
+            cost += e.cost;
+            profit += entry.profit;
+            current_value += entry.current_value;
+        });
+
+        summary.ticker = ticker;
+        summary.isFolded = isFolded;
+        summary.isFirst = isFirst;
+        summary.quantity = quantity;
+        summary.current_price = "-";
+        summary.profit = profit;
+        summary.current_value = current_value;
+        summary.profit_percentage = (100 + (profit / cost) * 100).toFixed(2);
+        summary.price = "-";
+        summary.cost = cost;
+
+        this.summary = summary;
+    }
+    // sort map entries by column specified as input arg
+    sort(columnName, inc) {
+        let sortDec;
+        let sortInc;
+        switch(columnName) {
+            case "ticker":
+                sortInc = (a, b) => { return a[0].localeCompare(b[0]); };
+                sortDec = (a, b) => { return b[0].localeCompare(a[0]); };
+                break;
+            case "quantity":
+                sortInc = (a, b) => { return a[1].average_data.quantity - b[1].average_data.quantity; };
+                sortDec = (a, b) => { return b[1].average_data.quantity - a[1].average_data.quantity; };
+                break;
+            case "price":
+                sortInc = (a, b) => { return a[1].average_data.price - b[1].average_data.price; };
+                sortDec = (a, b) => { return b[1].average_data.price - a[1].average_data.price; };
+                break;
+            case "current_price":
+                sortInc = (a, b) => { return a[1].current_price - b[1].current_price; };
+                sortDec = (a, b) => { return b[1].current_price - a[1].current_price; };
+                break;
+            case "current_value":
+                sortInc = (a, b) => { return a[1].current_price - b[1].current_price; };
+                sortDec = (a, b) => { return b[1].current_price - a[1].current_price; };
+                break;
+            case "profit":
+                sortInc = (a, b) => { return a[1].profit - b[1].profit; };
+                sortDec = (a, b) => { return b[1].profit - a[1].profit; };
+                break;
+            case "cost":
+                sortInc = (a, b) => { return a[1].current_value - b[1].current_value; }
+                sortDec = (a, b) => { return b[1].current_value - a[1].current_value; }
+            case "profit_percentage":
+                sortInc = (a, b) => { return a[1].profit_percentage - b[1].profit_percentage; };
+                sortDec = (a, b) => { return b[1].profit_percentage - a[1].profit_percentage; };
+                break;
+            default:
+                console.log("Default column name");
+                return;
+        }
+        const new_map = (inc) ? new Map([...this.asset_map].sort(sortInc)) : new Map([...this.asset_map].sort(sortDec));
+        this.asset_map = new_map;
+    }
+};
 
 function AddAssetButton({onAddPress}) {
     return (<div><button className="assets_add_button" onClick={onAddPress}>Add asset</button></div>);
 }
-
 
 function AssetsTableHead({ onSortUp, onSortDown }) {
     return (
@@ -118,8 +270,20 @@ function AssetsTableHead({ onSortUp, onSortDown }) {
             </th>
             <th>
                 <div className='table_header'>
+                    <span>Cost</span>
+                    <AssetsTableSortButtons onSortUp={onSortUp} onSortDown={onSortDown} columnName={"cost"}/>
+                </div>
+            </th>
+            <th>
+                <div className='table_header'>
                     <span>Current Price</span>
                     <AssetsTableSortButtons onSortUp={onSortUp} onSortDown={onSortDown} columnName={"current_price"}/>
+                </div>
+            </th>
+            <th>
+                <div className='table_header'>
+                    <span>Current Value</span>
+                    <AssetsTableSortButtons onSortUp={onSortUp} onSortDown={onSortDown} columnName={"current_value"}/>
                 </div>
             </th>
             <th>
@@ -149,59 +313,56 @@ function AssetsTableSortButtons({ onSortUp, onSortDown, columnName }) {
 }
 
 function AssetsTableBody({assets, onToggleVisibility}) {
-    // create list of elements from a assets map
-    const dummy_profit_percentage = 10;
-    var assets_array = [];
-
-    assets.forEach((asset) => {
-        const ticker = asset.ticker;
-        asset.data.forEach((d, idx) => {
-            const next_asset = {
-                isFolded:                   asset.folded,
-                isFirst:                    idx == 0,
-                ticker :                    ticker,
-                quantity:                   d.quantity,
-                current_price:              asset.current_price,
-                profit:                     d.getProfit(asset.current_price).toFixed(2),
-                profit_percentage:          d.getPercentageProfit(asset.current_price).toFixed(2),
-                price:                      d.price.toFixed(2), // display as string with .2 digit precision
-            };
-            assets_array.push(next_asset);    
-        });        
-    });
-
+    // prepare data for rendering table
+    const assets_array = assets.produceTableData();
+    const assets_summary = assets.summary;
     return (
         <>
+        {/* render regular rows */}
         {assets_array.map((asset, rowIdx) => (
-            <tr key={rowIdx}>
-                <td>
-                    <div className="row_ticker">
-                        <span style={{fontWeight: 'bold'}}>{asset.ticker}</span>
-                        {asset.isFirst && (
-                            <button onClick={()=> onToggleVisibility(asset.ticker)}>
-                                {asset.isFolded ? (<ArrowDownIcon/>) : (<ArrowUpIcon/>)}
-                            </button>
-                        )}
-                    </div>
-                </td>
-                <td>
-                    {asset.quantity}
-                </td>
-                <td>
-                    {asset.price}
-                </td>
-                <td>
-                    {asset.current_price}
-                </td>
-                <td>
-                    {asset.profit}
-                </td>
-                <td className="row_profit">
-                    {asset.profit_percentage}
-                </td>
-            </tr>
+            <AssetRow asset={asset} key={rowIdx} rowClassName={""} onToggleVisibility={onToggleVisibility}/>
         ))}
-        </>
+        {/* render summary row */}
+        <AssetRow asset={assets_summary} rowClassName={"assets_summary"} onToggleVisibility={onToggleVisibility}/>
+        </> 
+    );
+}
+
+function AssetRow({ asset, rowIdx, rowClassName, onToggleVisibility }) {
+    return (
+        <tr className={rowClassName}>
+            <td>
+                <div className="row_ticker">
+                    <span style={{fontWeight: 'bold'}}>{asset.ticker}</span>
+                    {asset.isFirst && (
+                        <button onClick={()=> onToggleVisibility(asset.ticker)}>
+                            {asset.isFolded ? (<ArrowDownIcon/>) : (<ArrowUpIcon/>)}
+                        </button>
+                    )}
+                </div>
+            </td>
+            <td>
+                {asset.quantity}
+            </td>
+            <td>
+                {asset.price}
+            </td>
+            <td>
+                {asset.cost}
+            </td>
+            <td>
+                {asset.current_price}
+            </td>
+            <td>
+                {asset.current_value}
+            </td>
+            <td>
+                {asset.profit}
+            </td>
+            <td className="row_profit">
+                {asset.profit_percentage}
+            </td>
+        </tr>
     );
 }
 
@@ -229,7 +390,7 @@ export default function StockPage() {
     t.insert(3, 7);
     t.current_price = 4;
 
-    let example_assets = new Map();
+    let example_assets = new AssetsMap();
     example_assets.set("a", a);
     example_assets.set("k", k);
     example_assets.set("t", t);
@@ -263,7 +424,7 @@ export default function StockPage() {
         }
 
 
-        var assets_map = new Map(assets);
+        var assets_map = new AssetsMap(assets);
         const map_entry = assets_map.get(ticker) ?? new AssetsEntry(ticker);
         map_entry.insert(quantityNum, priceNum);
         assets_map.set(ticker, 
@@ -280,55 +441,22 @@ export default function StockPage() {
             return;
         }
         // trigger visibility on resource
-        const map_copy = new Map(assets);
+        const map_copy = new AssetsMap(assets);
         let asset = map_copy.get(ticker);
         asset.triggerVisibility();
         setAssets(map_copy);
     }
-    // columnName (string), inc (bool, is sorting increasing)
-    function sortColumn(columnName, inc) {
-        let sorted_map;
-        let sortDec;
-        let sortInc;
-        switch(columnName) {
-            case "ticker":
-                sortInc = (a, b) => { return a[0].localeCompare(b[0]); };
-                sortDec = (a, b) => { return b[0].localeCompare(a[0]); };
-                break;
-            case "quantity":
-                sortInc = (a, b) => { return a[1].average_data.quantity - b[1].average_data.quantity; };
-                sortDec = (a, b) => { return b[1].average_data.quantity - a[1].average_data.quantity; };
-                break;
-            case "price":
-                sortInc = (a, b) => { return a[1].average_data.price - b[1].average_data.price; };
-                sortDec = (a, b) => { return b[1].average_data.price - a[1].average_data.price; };
-                break;
-            case "current_price":
-                sortInc = (a, b) => { return a[1].current_price - b[1].current_price; };
-                sortDec = (a, b) => { return b[1].current_price - a[1].current_price; };
-                break;
-            case "profit":
-                sortInc = (a, b) => { return a[1].profit - b[1].profit; };
-                sortDec = (a, b) => { return b[1].profit - a[1].profit; };
-                break;
-            case "profit_percentage":
-                sortInc = (a, b) => { return a[1].profit_percentage - b[1].profit_percentage; };
-                sortDec = (a, b) => { return b[1].profit_percentage - a[1].profit_percentage; };
-                break;
-            default:
-                console.log("Default column name");
-                return;
-        }
-        sorted_map = (inc) ? new Map([...assets].sort(sortInc)) : new Map([...assets].sort(sortDec));
-        setAssets(sorted_map);
-    }
 
     function sortUp(columnName) {
-        sortColumn(columnName, false);
+        const new_map = new AssetsMap(assets);
+        new_map.sort(columnName, false);
+        setAssets(new_map);
     }
 
     function sortDown(columnName) {
-        sortColumn(columnName, true);
+        const new_map = new AssetsMap(assets);
+        new_map.sort(columnName, true);
+        setAssets(new_map);
     }
 
     return (
