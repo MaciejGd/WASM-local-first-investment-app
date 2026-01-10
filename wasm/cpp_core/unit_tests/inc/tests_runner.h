5b7d7c96b8cf100b359f724cf6389a7b52037860
Maciej Gdula <maciekzgk@gmail.com>
2026-01-10 19:40:03 +0100

    Test runner and UNIT_TEST macro implemented

--- wasm/cpp_core/unit_tests/inc/tests_runner.h
+++ wasm/cpp_core/unit_tests/inc/tests_runner.h
@@ -6,10 +6,14 @@
 #include <map>
 #include <memory>
 #include <algorithm>
-
+#include <assert.h>
+#include <sstream>
 
 using std::vector;
 using std::string;
+
+
+namespace utests {
 
 /// @brief Information about test, testsuite name and testname
 class [[nodiscard]] TestInfo {
@@ -30,32 +34,26 @@ private:
 class [[nodiscard]] Test {
 public:
     Test(): info(TestInfo{"test", "test"}) {};
+    Test(const std::string& testsuite, const std::string& testname): info(TestInfo{testsuite, testname}) {};
     Test(Test&& other) = default;
     Test& operator=(Test&& other) = default;
     
-    void registerTest() {};
-    // TODO, run test
-    void Run() {};
-
     constexpr const std::string& GetTestsuite() const { return info.GetTestSuite(); }
     constexpr const std::string& GetTestName() const { return info.GetTestName(); }
 
-    virtual void TestBody() {};
+    /// @brief Object storing result of the test
+    struct TestResultContainer {
+        bool value = true;
+    };
+
+    virtual void Run(TestResultContainer& res) = 0;
 protected:
     TestInfo info;
 };
 
-
 /// @brief Main testrunner Singleton class
 class UnitTestRunner {
 public:
-    static UnitTestRunner* GetInstance() {
-        if (!instance) {
-            instance = new UnitTestRunner{};
-        }
-        return instance;
-    };
-
     /// @brief Register testcase for running
     /// @param test_ptr unique_ptr to the testclass
     /// @return bool, true on success, false otherwise
@@ -65,25 +63,25 @@ public:
         // if testsuite not registered yet, register it and add test to container
         if (test_map.find(testsuite) == test_map.end()) {
             test_map[testsuite].push_back(std::move(test_ptr));
-        }
-        // if testsuite exists check if test already exists in the container
-        const auto& testsuite_entry = test_map[testsuite];
-        auto found_test = std::find_if(testsuite_entry.begin(), testsuite_entry.end(), [testname](const auto& a) {
-            return (a->GetTestName() == testname);
-        });
-        if (found_test != testsuite_entry.end()) {
-            std::cout << "Test name: " << testname << " already added to the testsuite!\n";
-            return false;
+            return true;
         }
         // if not found, append test to the testsuite container
         test_map[testsuite].push_back(std::move(test_ptr));
+        return true;
     };
 
     /// @brief Run all registered testcases
     static void RunTests() {
         for (const auto& testsuite : test_map) {
             for (auto& test : testsuite.second) {
-                test->Run();
+                utests::Test::TestResultContainer res;
+                test->Run(res);
+                if (!res.value) {
+                    std::cout << "Testcase " << test->GetTestsuite() << ":" << test->GetTestName() << " failed!\n";
+                }
+                else {
+                    std::cout << "Testcase " << test->GetTestsuite() << ":" << test->GetTestName() << " passed!\n";
+                }
             }
         }
     };
@@ -94,14 +92,31 @@ private:
     static UnitTestRunner* instance;
 
     // map of string to vector of Tests
-    static std::map<std::string, std::vector<std::unique_ptr<Test>>> test_map;
+    inline static std::map<std::string, std::vector<std::unique_ptr<Test>>> test_map;
 };
 
 
 #define UNIT_TEST(test_suite, test_name)\ 
-        class test_suite##_##test_name##_Test : public Test {\
+        class test_suite##_##test_name##_Test : public utests::Test {\
         public:\
-            test_suite##_##test_name##_Test(): Test() {};\
-            void TestBody() override {}; \
+            test_suite##_##test_name##_Test(): utests::Test(#test_suite, #test_name) {};\
+            void Run(utests::Test::TestResultContainer& res) override; \
         };\
+\
+        static bool test_suite##_##test_name##_Test_registering_result = utests::UnitTestRunner::RegisterTest(\
+            std::make_unique<test_suite##_##test_name##_Test>()\
+        );\
+\
+        void test_suite##_##test_name##_Test::Run(utests::Test::TestResultContainer& res)
+
+};
+
+/// @brief Use for integers
+#define CHECK_EQUAL(a,b)\
+    if (a != b) res.value = false; return;
+
+/// @brief Use for floating point numbers
+#define CHECK_NEAR(a,b)\
+    if (a - b <= -0.000001 || a - b >= 0.000001) res.value = false; return;
+    
 
--- wasm/cpp_core/unit_tests/main.cpp
+++ wasm/cpp_core/unit_tests/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include "./inc/tests_runner.h"
 
+#define TEST_FAIL()\
+    res.value = false;\
+    return;\
+
+
 //using namespace unit_tests;
+UNIT_TEST(Testsuite, testing1) 
+{
+    CHECK_NEAR(1.0000001, 1.0000002);
+}
+
+//using namespace unit_tests;
+UNIT_TEST(Testsuite, testing2) 
+{
+    CHECK_NEAR(1.0001, 1.020);
+}
 
 int main() {
-    UNIT_TEST(Testsuite, testing);
+    utests::UnitTestRunner::RunTests();
     return 0;
 }
 
