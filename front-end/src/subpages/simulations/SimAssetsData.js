1f326efec84b497a56f5ccd7475866fb1e1c8671
Maciej Gdula <maciekzgk@gmail.com>
2026-02-22 20:34:01 +0100

    Fixed bug with adding record to the SimAssetsMap

--- front-end/src/subpages/simulations/SimAssetsData.js
+++ front-end/src/subpages/simulations/SimAssetsData.js
@@ -11,15 +11,17 @@ export class SimAssetMap {
     addRecord(ticker, price) {
         // at first check if price can be turned into a number
         if (isNaN(Number(price))) {
+            console.log("Price is not a number!!!");
             return false; 
         }
+
         var el = this.asset_map.get(ticker);
         // if already exists        
-        if (el != undefined) {
-            this.asset_map.set(ticker, Number(price));
+        if (el !== undefined) {
+            this.asset_map.set(ticker, el += Number(price));
         }
         else {
-            this.asset_map.set(ticker, el += Number(price));
+            this.asset_map.set(ticker, Number(price));
         }
 
         return true;
@@ -31,11 +33,10 @@ export class SimAssetMap {
             sum += value;
         });
         var array = Array.from(this.asset_map);
-        for (var i = 0; i < array.length; i++) {
-            // second element of array would be price
-            let el = array[i][1];
+        for (var i = 0; i < array.length; i++) {            
             // append percentage value of each asset
-            array[i].push(Number(el) / sum);
+            let percent = (array[i][1] / sum) * 100;
+            array[i].push(percent.toFixed(2));
         }
         return array;
     }
