c819745adac9201cec393ee666107b9de81abba4
Maciej Gdula <maciekzgk@gmail.com>
2026-07-09 21:58:33 +0200

    Fixing sync test issues

--- backend/tests/data.sql
+++ backend/tests/data.sql
@@ -1,8 +1,7 @@
-INSERT INTO user (username, password)
+INSERT INTO user (username, password, salt)
 VALUES 
-('test', 'test'),
--- ('test', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
-  ('other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79');
+('testing', 'testing', 'test_salt_value'),
+('other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79', 'other_salt_value');
 
 -- INSERT INTO post (title, body, author_id, created)
 -- VALUES 
--- backend/tests/integration/test_register.py
+++ backend/tests/integration/test_register.py
@@ -6,7 +6,7 @@ from http import HTTPStatus
 
 def test_register_user(app, authentication):
     response = authentication.register("test", "pass")
-    response.status_code == HTTPStatus.OK
+    assert response.status_code == HTTPStatus.OK
 
     cursor = get_user_from_db(app, "test")
     if not cursor:
@@ -20,4 +20,4 @@ def test_register_user_username_already_used(app, authentication):
     add_user(app, "test", "pass", "salt")
 
     response = authentication.register("test", "pass")
-    response.status_code == HTTPStatus.CONFLICT
+    assert  response.status_code == HTTPStatus.CONFLICT
